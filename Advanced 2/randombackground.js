51c84db109d208a853b5327e9bd0adb2e781fe81
Katharina Simma <katharina.simma@diebin.at>
2020-09-22 15:14:55 +0200

    advanced 2 first draft

---
+++ Advanced 2/randombackground.js
@@ -0,0 +1,22 @@
+//let body = document.getElementsByTagName(body);
+
+function randomBackground(){
+    var RGB1 = Math.floor((Math.random() * 255));
+    var RGB2 = Math.floor((Math.random() * 255));
+    var RGB3 = Math.floor((Math.random() * 255));   
+   document.body.style.backgroundColor = "rgb(RGB1, RGB2, RGB3)";
+}
+
+randomBackground();
+
+var RGB1 = Math.floor((Math.random() * 255));
+var RGB2 = Math.floor((Math.random() * 255));
+var RGB3 = Math.floor((Math.random() * 255));
+
+document.write(RGB1 + "<br>");
+document.write(RGB2 + "<br>");
+document.write(RGB3 + "<br>");
+
+
+
+//rgb(255, 0, 0)
